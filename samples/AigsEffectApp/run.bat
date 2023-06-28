efba5bfe9c294e4b5111cb7e680b7ed728f859ae
Sen <alvrtsyan.seno@gmail.com>
2023-06-28 16:52:23 +0400

    Project Cleanup

--- samples/AigsEffectApp/run.bat
+++
@@ -1,4 +0,0 @@
-SETLOCAL
-SET PATH=%PATH%;..\external\opencv\bin;
-REM AigsEffectApp.exe --in_file=..\input\input_003054.jpg --show
-AigsEffectApp.exe --webcam --show
--- samples/DenoiseEffectApp/run.bat
+++
@@ -1,4 +0,0 @@
-SETLOCAL
-SET PATH=%PATH%;..\external\opencv\bin;
-DenoiseEffectApp.exe --webcam --strength=0 --show
-DenoiseEffectApp.exe --webcam --strength=1 --show
